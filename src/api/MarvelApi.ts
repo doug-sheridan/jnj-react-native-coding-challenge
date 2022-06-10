c93b08cf02957c71f64e19aff5cce68fa965c43d
Douglas Sheridan <doug@zenifydesignstudio.com>
2022-06-09 20:52:53 -0700

    stub out marvel api constants

---
+++ src/api/MarvelApi.ts
@@ -0,0 +1,10 @@
+export const MARVEL_API_CREDENTIALS = {
+  publicKey: "8f5b67b14b73bafdabd335bc60ef0c1f",
+  privateKey: "9aef5836e9594906b65f5a55242d93ec807bf3b8",
+};
+
+export const MARVEL_BASE_URL = "https://gateway.marvel.com:443/v1/public/";
+
+export const MARVEL_API_ROUTES = {
+  characters: 'characters',
+};
